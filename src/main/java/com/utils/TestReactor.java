6e98c0335b724ebb91c871931d620e11a93d3825
Dylan Duan <dylanduan@bc.group>
2019-04-24 11:02:24 +0800

    add test for disruptor and conuntDownLatch

---
+++ src/main/java/com/utils/TestReactor.java
@@ -0,0 +1,17 @@
+package com.utils;
+
+import reactor.core.publisher.Mono;
+
+public class TestReactor {
+
+
+    public static void main(String[] args){
+        Mono<String> just = Mono.just("foo");
+
+
+        just.subscribe(text -> System.out.println(text));
+
+
+    }
+
+}
